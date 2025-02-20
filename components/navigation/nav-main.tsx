88f9089e95c5a59f2b2cde401fbd4b4dc61d1565
Robert-Lexicon <robert.hedblad@lexicon.se>
2025-02-20 11:37:01 +0100

    Added main menu

---
+++ components/navigation/nav-main.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "../ui/navigation-menu";
+
+export default function NavMain() {
+  return (
+    // Root/nav for menu
+    <NavigationMenu>
+      {/* becomes ul in menu */}
+      <NavigationMenuList>
+        {/* becomes li  */}
+        <NavigationMenuItem>
+          <Link href="/" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Home
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/about" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              About
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
