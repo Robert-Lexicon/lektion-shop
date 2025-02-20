8343f5bcb47c91848a44acadbb2b571fdbee3ec5
Robert-Lexicon <robert.hedblad@lexicon.se>
2025-02-20 12:16:00 +0100

    Refactored out product list and cards for list and details + added suspense for streaming fallback

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import Image from "next/image";
-import Link from "next/link";
 import { fetchCategories, fetchProductsByCategory } from "./actions";
 import CategorySelect from "@/components/navigation/category-select";
-import Ratings from "@/components/ui/ratings";
+import { Products } from "@/components/products";
+import { Suspense } from "react";
 
 //todo make interface for searchParams
 export default async function Home({
@@ -15,38 +13,17 @@ export default async function Home({
   //get category key/value from searchParams, if none, default to electronics
   const { category = "electronics" } = await searchParams;
 
-  const data = await fetchProductsByCategory(category);
+  const products = fetchProductsByCategory(category);
   const categories = fetchCategories();
-  console.log(data);
+
   return (
     <main className="space-y-4">
-      <CategorySelect categories={categories} />
-      <ul className="grid grid-cols-[repeat(auto-fit,minmax(16rem,1fr))] gap-4">
-        {data.map((product) => (
-          <li key={product.id}>
-            <Link href={`/product/${product.id}`}>
-              <Card className="grid  h-full">
-                <CardHeader className="grid justify-items-center text-center space-y-4">
-                  <Image
-                    className="w-full max-w-[12rem] max-h-[200px] object-contain"
-                    src={product.image}
-                    height={100}
-                    width={100}
-                    alt=""
-                  />
-                  <Ratings rating={product.rating.rate} />
-                  <CardTitle>
-                    <h3 className="font-semibold text-lg">{product.title}</h3>
-                  </CardTitle>
-                </CardHeader>
-                <CardContent className="text-center">
-                  <p className="text-xl font-semibold">${product.price}</p>
-                </CardContent>
-              </Card>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <Suspense fallback={<div>Loading...</div>}>
+        <CategorySelect categories={categories} />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Products products={products} />
+      </Suspense>
     </main>
   );
 }
--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,14 +1,6 @@
 import { fetchProduct } from "@/app/actions";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import Ratings from "@/components/ui/ratings";
-import { Product } from "@/lib/interfaces";
-import Image from "next/image";
+import { ProductDetailsCard } from "@/components/products/product-details-card";
+import { Suspense } from "react";
 
 //get the dynamic id from the page url and use that to fetch products
 export default async function ProductPage({
@@ -18,32 +10,10 @@ export default async function ProductPage({
 }) {
   //since params is a promise we have to await it first
   const { id } = await params;
-  const data: Product = await fetchProduct(id);
+  const data = fetchProduct(id);
   return (
-    <Card>
-      <CardHeader className="text-center">
-        <CardTitle>
-          <h1 className="font-bold text-xl">{data.title}</h1>
-        </CardTitle>
-        <CardDescription>Category: {data.category}</CardDescription>
-      </CardHeader>
-
-      <CardContent className="space-y-4 grid grid-cols-2">
-        <Image
-          className="w-full max-w-[260px] px-4 object-contain"
-          src={data.image}
-          height={100}
-          width={100}
-          alt=""
-        />
-        <div className="grid gap-2 px-4">
-          <div className="flex gap-2 items-center">
-            <span>Rating:</span> <Ratings rating={data.rating.rate} />
-          </div>
-          <p className="text-pretty">{data.description}</p>
-          <p className="text-end text-4xl font-semibold">${data.price}</p>
-        </div>
-      </CardContent>
-    </Card>
+    <Suspense fallback={<div>Loading...</div>}>
+      <ProductDetailsCard product={data} />
+    </Suspense>
   );
 }
